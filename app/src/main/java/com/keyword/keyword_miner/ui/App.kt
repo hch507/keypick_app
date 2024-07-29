ea9ac806d741e5e22f6b4dc5406d15fceb94f6b0
Han Chunghee <96819679+hch507@users.noreply.github.com>
2024-07-29 21:08:55 +0900

    dark mode 비활성화

--- app/src/main/java/com/keyword/keyword_miner/ui/App.kt
+++ app/src/main/java/com/keyword/keyword_miner/ui/App.kt
@@ -2,6 +2,7 @@ package com.keyword.keyword_miner.ui
 
 import android.app.Application
 import android.content.Context
+import androidx.appcompat.app.AppCompatDelegate
 import com.keyword.keyword_miner.data.local.sharePref.PreferenceUtil
 import dagger.hilt.android.HiltAndroidApp
 
@@ -17,6 +18,7 @@ class App:Application() {
     override fun onCreate()
     {
         super.onCreate()
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         instance = this
     }
 
